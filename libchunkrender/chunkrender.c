7bb277504eba6cd2a55088fd543948980f1f6254
Petter Arvidsson <petter.arvidsson@gmail.com>
2017-12-29 20:59:25 +0100

    Refactor

--- libchunkrender/chunkrender.c
+++ libchunkrender/chunkrender.c
@@ -1,19 +1,47 @@
 #include <stdlib.h>
 #include "chunkrender.h"
 
-chunk_block_model_t allocate_chunk_block_model() {
+typedef struct {
+    uint8_t x;
+    uint8_t y;
+    uint8_t z;
+} point_t;
+
+static point_t point_in_slice(uint8_t face, uint8_t slice, uint8_t a, uint8_t b) {
+    point_t point;
+    if(face == 0 || face == 1) {
+        point.x = slice;
+        point.y = b;
+        point.z = a;
+    } else if(face == 2 || face == 3) {
+        point.x = a;
+        point.y = slice;
+        point.z = b;
+    } else {
+        point.x = a;
+        point.y = b;
+        point.z = slice;
+    }
+    return point;
+}
+
+static uint32_t chunk_index(point_t point) {
+    return point.x + point.y * CHUNK_SIZE + point.z * CHUNK_SIZE * CHUNK_SIZE;
+}
+
+static chunk_block_model_t allocate_chunk_block_model() {
     size_t vertices = 36;
     chunk_block_model_t model = {vertices, (uint32_t*)malloc(2*sizeof(uint32_t)*vertices)};
     return model;
 }
 
-chunk_block_model_t reallocate_chunk_block_model(chunk_block_model_t old_model) {
+static chunk_block_model_t reallocate_chunk_block_model(chunk_block_model_t old_model) {
     size_t vertices = old_model.vertices * 2;
     chunk_block_model_t model = {vertices, (uint32_t*)realloc(old_model.data, 2*sizeof(uint32_t)*vertices)};
     return model;
 }
 
-void free_chunk_block_model(chunk_block_model_t model) {
+static void free_chunk_block_model(chunk_block_model_t model) {
     free(model.data);
 }
 
@@ -54,6 +82,29 @@ static uint8_t uv[6][2] = {
 };
 
 
+void render_face(point_t point, uint8_t face, uint32_t texture, uint32_t *data) {
+    uint8_t du = texture % 16;
+    uint8_t dv = texture / 16;
+    for(uint8_t vertex = 0; vertex < 6; vertex++) {
+        data[vertex*2] =
+            (face << OFF_NORMAL) +
+            (face_vertices[face][vertex] << OFF_VERTEX) +
+            (point.x << OFF_X) +
+            (point.y << OFF_Y) +
+            (point.z << OFF_Z) +
+            (0 << OFF_AO) +
+            (0 << OFF_DAMAGE_U) +
+            (0 << OFF_DAMAGE_V);
+
+        data[vertex*2 + 1] =
+            ((du + uv[vertex][0]) << OFF_DU) +
+            ((dv + uv[vertex][1]) << OFF_DV) +
+            (0x0F << OFF_AL) +
+            (0 << OFF_R) + (0 << OFF_G) +
+            (0 << OFF_B) + (0 << OFF_LIGHT);
+    }
+}
+
 chunk_block_model_t render_chunk_blocks(uint8_t *data, uint8_t *is_transparent, uint8_t *state,
                                         uint32_t block_texture[][6]) {
     size_t vertices = 0;
@@ -62,45 +113,13 @@ chunk_block_model_t render_chunk_blocks(uint8_t *data, uint8_t *is_transparent, 
         for(uint8_t slice = 0; slice < CHUNK_SIZE; slice++) {
             for(uint8_t a = 0; a < CHUNK_SIZE; a++) {
                 for(uint8_t b = 0; b < CHUNK_SIZE; b++) {
-
-                    uint8_t x;
-                    uint8_t y;
-                    uint8_t z;
-                    if(face == 0 || face == 1) {
-                        x = slice;
-                        y = b;
-                        z = a;
-                    } else if(face == 2 || face == 3) {
-                        x = a;
-                        y = slice;
-                        z = b;
-                    } else {
-                        x = a;
-                        y = b;
-                        z = slice;
-                    }
-                    uint32_t i = x + y * CHUNK_SIZE + z * CHUNK_SIZE * CHUNK_SIZE;
+                    point_t point = point_in_slice(face, slice, a, b);
+                    uint32_t i = chunk_index(point);
                     uint32_t block_type = data[i*BLOCK_SIZE + BLOCKS_HEADER_SIZE];
                     if(state[block_type] != STATE_GAS) {
                         uint32_t texture = block_texture[block_type][face];
-                        uint8_t du = texture % 16;
-                        uint8_t dv = texture / 16;
-                        for(uint8_t vertex = 0; vertex < 6; vertex++) {
-                            model.data[vertices*2] =
-                                (face << OFF_NORMAL) +
-                                (face_vertices[face][vertex] << OFF_VERTEX) +
-                                (x << OFF_X) + (y << OFF_Y) + (z << OFF_Z) +
-                                (0 << OFF_AO) + (0 << OFF_DAMAGE_U) +
-                                (0 << OFF_DAMAGE_V);
-
-                            model.data[vertices*2 + 1] =
-                                ((du + uv[vertex][0]) << OFF_DU) +
-                                ((dv + uv[vertex][1]) << OFF_DV) +
-                                (0x0F << OFF_AL) +
-                                (0 << OFF_R) + (0 << OFF_G) +
-                                (0 << OFF_B) + (0 << OFF_LIGHT);
-                            vertices++;
-                        }
+                        render_face(point, face, texture, model.data + vertices * 2);
+                        vertices += 6;
                         if(vertices >= model.vertices) {
                             model = reallocate_chunk_block_model(model);
                         }
